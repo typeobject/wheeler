5a75e4da9e74c9d3fa843eb90ed420f53391e1f2
Jon Bringhurst <jon@bringhurst.org>
2024-11-24 22:12:30 -0800

    Fix up docusaurus config for custom site

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://typeobject.github.io',
+  url: 'https://wheeler.typeobject.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wheeler',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
