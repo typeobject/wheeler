5292dd3cdee43710e53ace8fb02dede3aa866caf
Jon Bringhurst <jon@bringhurst.org>
2026-08-05 14:53:36 -0700

    Separate seven-module fork role planning

--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
@@ -33,7 +33,7 @@ class NativeSha256ExampleTest {
       transitions += 1;
     }
 
-    assertEquals(14_452_708, transitions);
+    assertEquals(14_526_288, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(input), machine.hostOutput());
   }
