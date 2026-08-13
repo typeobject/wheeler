27d9216146e67bf41dc4517a5cbdc9c14dca2d58
Jon Bringhurst <jon@bringhurst.org>
2026-08-12 23:50:11 -0700

    Refresh formatted closure transition count

--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeBootstrapModulesIdentityExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeBootstrapModulesIdentityExampleTest.java
@@ -44,7 +44,7 @@ final class NativeBootstrapModulesIdentityExampleTest {
       transitions += 1;
     }
 
-    assertEquals(55_941_488, transitions);
+    assertEquals(55_941_424, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(manifest.canonicalBytes()),
         machine.hostOutput());
