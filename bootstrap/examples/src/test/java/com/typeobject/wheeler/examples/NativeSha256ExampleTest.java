75e08da4dfe355e122dda83ed82ef07842fd8888
Jon Bringhurst <jon@bringhurst.org>
2026-08-13 08:26:23 -0700

    Refresh scalar loop closure metrics

--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
@@ -33,7 +33,7 @@ class NativeSha256ExampleTest {
       transitions += 1;
     }
 
-    assertEquals(26_955_970, transitions);
+    assertEquals(27_053_978, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(input), machine.hostOutput());
   }
