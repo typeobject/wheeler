dd3d4c37153defe814e1e78644d6283177a954ec
Jon Bringhurst <jon@bringhurst.org>
2026-08-14 04:26:47 -0700

    Refresh compiler closure identities

--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
@@ -33,7 +33,7 @@ class NativeSha256ExampleTest {
       transitions += 1;
     }
 
-    assertEquals(28_523_258, transitions);
+    assertEquals(28_548_102, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(input), machine.hostOutput());
   }
