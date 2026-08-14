f070ef83edad7a9430a3ed034f1c216bc1ca36f4
Jon Bringhurst <jon@bringhurst.org>
2026-08-14 10:00:46 -0700

    Refresh compiler closure bounds

--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeBootstrapModulesIdentityExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeBootstrapModulesIdentityExampleTest.java
@@ -35,7 +35,7 @@ final class NativeBootstrapModulesIdentityExampleTest {
   void validatesThePhysicalBoundedCompilerClosure() throws Exception {
     BootstrapModuleManifest manifest = CompilerSources.bootstrapModuleManifest();
 
-    assertEquals(149_125, manifest.canonicalBytes().length);
+    assertEquals(149_212, manifest.canonicalBytes().length);
     VirtualMachine machine = vm(program(), manifest.canonicalBytes());
     long transitions = 0;
     while (machine.status() != MachineStatus.HALTED
@@ -44,13 +44,13 @@ final class NativeBootstrapModulesIdentityExampleTest {
       transitions += 1;
     }
 
-    assertEquals(59_959_641, transitions);
+    assertEquals(60_000_355, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(manifest.canonicalBytes()),
         machine.hostOutput());
     assertEquals(330, machine.global("moduleCount"));
     assertEquals(2, machine.global("externalCount"));
-    assertEquals(1_581, machine.global("importCount"));
+    assertEquals(1_583, machine.global("importCount"));
     assertEquals(1, machine.global("published"));
   }
 
--- bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
+++ bootstrap/examples/src/test/java/com/typeobject/wheeler/examples/NativeSha256ExampleTest.java
@@ -33,7 +33,7 @@ class NativeSha256ExampleTest {
       transitions += 1;
     }
 
-    assertEquals(28_548_102, transitions);
+    assertEquals(28_560_164, transitions);
     assertEquals(MachineStatus.HALTED, machine.status());
     assertArrayEquals(MessageDigest.getInstance("SHA-256").digest(input), machine.hostOutput());
   }
